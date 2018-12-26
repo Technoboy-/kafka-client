24854201cd8ee43daee811c123aee4f6cb2ad973
dk.technoboy <guo.jiwei@immomo.com>
2018-12-26 20:14:43 +0800

    fix bug

--- src/main/java/com/tt/kafka/consumer/service/BatchAcknowledgeMessageListenerService.java
+++ src/main/java/com/tt/kafka/consumer/service/BatchAcknowledgeMessageListenerService.java
@@ -5,21 +5,23 @@ import com.tt.kafka.consumer.DefaultKafkaConsumerImpl;
 import com.tt.kafka.consumer.Record;
 import com.tt.kafka.consumer.listener.BatchAcknowledgeMessageListener;
 import com.tt.kafka.consumer.listener.MessageListener;
-import com.tt.kafka.metric.Monitor;
 import com.tt.kafka.metric.MonitorImpl;
+import com.tt.kafka.util.NamedThreadFactory;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 /**
  * @Author: Tboy
  */
-public class BatchAcknowledgeMessageListenerService<K, V> extends RebalanceAcknowledgeMessageListenerService<K, V>{
+public class BatchAcknowledgeMessageListenerService<K, V> extends RebalanceAcknowledgeMessageListenerService<K, V> implements Runnable{
 
     private static final Logger LOG = LoggerFactory.getLogger(BatchAcknowledgeMessageListenerService.class);
 
@@ -35,12 +37,17 @@ public class BatchAcknowledgeMessageListenerService<K, V> extends RebalanceAckno
 
     private final Semaphore semaphore = new Semaphore(1);
 
+    private final String schedulerThreadName = "scheduler-thread";
+
+    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor(new NamedThreadFactory(schedulerThreadName));
+
     public BatchAcknowledgeMessageListenerService(DefaultKafkaConsumerImpl<K, V> consumer, MessageListener<K, V> messageListener) {
         super(consumer);
         this.messageListener = (BatchAcknowledgeMessageListener)messageListener;
         this.batchConsumeSize = consumer.getConfigs().getBatchConsumeSize();
         this.batchConsumeTime = TimeUnit.SECONDS.toMillis(consumer.getConfigs().getBatchConsumeTime());
         this.container = new ArrayList<>(this.batchConsumeSize);
+        executor.scheduleAtFixedRate(this, batchConsumeTime, batchConsumeTime, TimeUnit.SECONDS);
         MonitorImpl.getDefault().recordConsumeHandlerCount(1);
     }
 
@@ -48,11 +55,17 @@ public class BatchAcknowledgeMessageListenerService<K, V> extends RebalanceAckno
     public void onMessage(final ConsumerRecord<byte[], byte[]> record) {
         long now = System.currentTimeMillis();
         try {
-            container.add(consumer.toRecord(record));
+            if(!(record instanceof EmptyConsumerRecord)){
+                container.add(consumer.toRecord(record));
+            }
             if(container.size() < this.batchConsumeSize || System.currentTimeMillis() + batchConsumeTime < lastConsumeTime){
                 return;
             }
-            semaphore.acquire();
+            if(Thread.currentThread().getName().startsWith(schedulerThreadName) && !semaphore.tryAcquire()){
+                return;
+            } else{
+                semaphore.acquire();
+            }
             final List<Record<K, V>> records = this.container;
             this.container = new ArrayList<>(this.batchConsumeSize);
             messageListener.onMessage(records, new BatchAcknowledgeMessageListener.Acknowledgment() {
@@ -70,6 +83,20 @@ public class BatchAcknowledgeMessageListenerService<K, V> extends RebalanceAckno
         } finally {
             MonitorImpl.getDefault().recordConsumeProcessCount(this.batchConsumeSize);
             MonitorImpl.getDefault().recordConsumeProcessTime(System.currentTimeMillis() - now);
+        }
+    }
+
+    @Override
+    public void run(){
+        onMessage(EmptyConsumerRecord.EMPTY);
+    }
+
+    static class EmptyConsumerRecord extends ConsumerRecord<byte[], byte[]>{
+
+        static EmptyConsumerRecord EMPTY = new EmptyConsumerRecord("", 0, 0, null, null);
+
+        public EmptyConsumerRecord(String topic, int partition, long offset, byte[] key, byte[] value) {
+            super(topic, partition, offset, key, value);
         }
     }
 
