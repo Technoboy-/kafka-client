4dd71d8d90806d901a6a198e0839216e56be4611
dk.technoboy <guo.jiwei@immomo.com>
2019-01-23 19:17:45 +0800

    updates

--- src/main/java/com/owl/kafka/client/proxy/transport/handler/ClientHandler.java
+++ src/main/java/com/owl/kafka/client/proxy/transport/handler/ClientHandler.java
@@ -3,6 +3,8 @@ package com.owl.kafka.client.proxy.transport.handler;
 import com.owl.kafka.client.proxy.transport.Connection;
 import com.owl.kafka.client.proxy.transport.NettyConnection;
 import com.owl.kafka.client.proxy.transport.protocol.Packet;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelConfig;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -23,16 +25,43 @@ public class ClientHandler extends ChannelInboundHandlerAdapter {
         this.dispatcher = dispatcher;
     }
 
+    @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
         Connection connnection = NettyConnection.attachChannel(ctx.channel());
         connnection.close();
     }
 
+    @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         dispatcher.dispatch(NettyConnection.attachChannel(ctx.channel()), (Packet)msg);
     }
 
+    @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         LOGGER.error("exceptionCaught", cause);
+        ctx.close();
     }
+
+    @Override
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+        Channel ch = ctx.channel();
+        ChannelConfig config = ch.config();
+
+        if (!ch.isWritable()) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn("{} is not writable, high water mask: {}, the number of flushed entries that are not written yet: {}.",
+                        new Object[]{ch, config.getWriteBufferHighWaterMark(), ch.unsafe().outboundBuffer().size()});
+            }
+
+            config.setAutoRead(false);
+        } else {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn("{} is writable(rehabilitate), low water mask: {}, the number of flushed entries that are not written yet: {}.",
+                        new Object[]{ch, config.getWriteBufferLowWaterMark(), ch.unsafe().outboundBuffer().size()});
+            }
+
+            config.setAutoRead(true);
+        }
+    }
+
 }
